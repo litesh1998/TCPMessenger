28ea4f1b9d57a2449042ade7fb2fe597e447291c
litesh1998 <litesh1998@gmail.com>
2020-11-13 16:28:02 +0530

    formatted

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ from sys import exit
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(("127.0.0.1", 55556))
 
-nickname=input('Pls Enter a nickname')
+nickname = input('Pls Enter a nickname')
+
 
 def recive():
     while True:
         try:
-            message=server.recv(1024).decode("ascii")
+            message = server.recv(1024).decode("ascii")
             if message == "NICK":
                 server.send(nickname.encode('ascii'))
             else:
@@ -20,17 +21,19 @@ def recive():
             server.close()
             break
 
+
 def write():
     while True:
         try:
-            message=f'{nickname}: {input("")}'
+            message = f'{nickname}: {input("")}'
             server.send(message.encode('ascii'))
         except:
             server.close()
             exit()
 
-recive_thread=threading.Thread(target=recive)
+
+recive_thread = threading.Thread(target=recive)
 recive_thread.start()
 
-write_thread= threading.Thread(target=write)
+write_thread = threading.Thread(target=write)
 write_thread.start()
